ddc2050b0668fad508d9b1aa0bfc9efd2a9ce74c
Amruth L S <lsamruth@gmail.com>
2021-03-19 18:24:16 +0530

    handle highlight missed

--- src/App.js
+++ src/App.js
@@ -7,13 +7,22 @@ const convertFirstCharToUpperCase = (str) => {
   }
 };
 
+const handleHighlight = (event) =>{
+  if(event){
+    event.target.setAttribute("style", "border:3px solid yellow");
+    setTimeout(()=>{
+      event.target.removeAttribute("style");
+    },200)
+  }
+}
 function App() {
-  const playSound = (src) => {
+  const playSound = (src,event) => {
     let audio = new Audio(src.audio);
     audio.play();
+    handleHighlight(event);
   };
   let drums = drumData.map((i) => (
-    <div className="key" key={i.key} onClick={() => playSound(i)}>
+    <div className="key" key={i.key} onClick={(e) => playSound(i,e)}>
       {i.key}
       <p className="audio">{convertFirstCharToUpperCase(i.name)}</p>
     </div>
